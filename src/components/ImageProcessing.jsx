ea7729eabd2ad78cf4eea73e42048b9e354ae4a1
Cameron Doyle <cameron.doyle465@gmail.com>
2023-01-02 20:17:49 +1100

    made some book marks and cleaned up some debug logging

--- src/components/ImageProcessing.jsx
+++ src/components/ImageProcessing.jsx
@@ -39,10 +39,10 @@ export default function ImageProcessing(props) {
 		//imgBlob = await applyRatio(imgBlob, 1.333)
 
 		//Scale imgBlob
-		//imgBlob = await scale(imgBlob, 1024)
+		imgBlob = await scale(imgBlob, 720)
 
 		//Compress img
-		const compressedFile = await compress(imgBlob, 10)
+		const compressedFile = await compress(imgBlob, 40)
 
 		//Get new dimensions
 		const dimensions = await getDimensions(compressedFile)
@@ -106,42 +106,44 @@ async function getImageData(imgBlob) {
 /**
  * Compresses an image Blob
  * @param {Blob} imgBlob 
- * @param {Number} quality 
+ * @param {object} settings 
  * @returns {Promise<Blob>} compressed image blob
  */
-async function compress(imgBlob, quality = 75) {
+async function compress(imgBlob, quality) {
+	//TODO override webpDefaults with webpSettings
+	const webpDefaults = {
+		quality,
+		target_size: 0,
+		target_PSNR: 0,
+		method: 4,
+		sns_strength: 50,
+		filter_strength: 60,
+		filter_sharpness: 0,
+		filter_type: 1,
+		partitions: 0,
+		segments: 4,
+		pass: 1,
+		show_compressed: 0,
+		preprocessing: 0,
+		autofilter: 0,
+		partition_limit: 0,
+		alpha_compression: 1,
+		alpha_filtering: 1,
+		alpha_quality: 100,
+		lossless: 0,
+		exact: 0,
+		image_hint: 0,
+		emulate_jpeg_size: 0,
+		thread_level: 0,
+		low_memory: 0,
+		near_lossless: 100,
+		use_delta_palette: 0,
+		use_sharp_yuv: 0,
+	}
 	return new Promise(async resolve => {
 		webp_enc().then(async module => {
 			const image = await getImageData(imgBlob);
-			resolve(new Blob([module.encode(image.data, image.width, image.height, {
-				quality,
-				target_size: 0,
-				target_PSNR: 0,
-				method: 4,
-				sns_strength: 50,
-				filter_strength: 60,
-				filter_sharpness: 0,
-				filter_type: 1,
-				partitions: 0,
-				segments: 4,
-				pass: 1,
-				show_compressed: 0,
-				preprocessing: 0,
-				autofilter: 0,
-				partition_limit: 0,
-				alpha_compression: 1,
-				alpha_filtering: 1,
-				alpha_quality: 100,
-				lossless: 0,
-				exact: 0,
-				image_hint: 0,
-				emulate_jpeg_size: 0,
-				thread_level: 0,
-				low_memory: 0,
-				near_lossless: 100,
-				use_delta_palette: 0,
-				use_sharp_yuv: 0,
-			})], { type: 'image/webp' }));
+			resolve(new Blob([module.encode(image.data, image.width, image.height, webpDefaults)], { type: 'image/webp' }));
 		})
 	})
 }
@@ -221,8 +223,6 @@ async function scale(imgBlob, px) {
 
 		canvas.width = dimensions.width - (dimensions.width * scale)
 		canvas.height = dimensions.height - (dimensions.height * scale)
-		console.log("Scale", scale)
-		console.log("Scaling", canvas.width, canvas.height)
 
 		const img = new Image()
 
